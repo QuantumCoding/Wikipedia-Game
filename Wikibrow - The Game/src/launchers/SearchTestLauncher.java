8efb17b955c7f2e0584f4784faac4f4336e3902d
Joshua <JCilfone@gmail.com>
2017-07-27 08:05:21 -0400

    Re-added JTidy Library

---
+++ Wikibrow - The Game/src/launchers/SearchTestLauncher.java
@@ -0,0 +1,9 @@
+package launchers;
+
+import window.browser.WikipediaSearch;
+
+public class SearchTestLauncher {
+	public static void main(String[] args) {
+		System.out.println(WikipediaSearch.lookUp("Taco"));
+	}
+}
--- Wikibrow - The Game/src/window/browser/WikipediaSearch.java
+++ Wikibrow - The Game/src/window/browser/WikipediaSearch.java
@@ -12,6 +12,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.tidy.Tidy;
 
 public class WikipediaSearch {
 	private static final String GOOGLE_ROOT_URL = "https://www.google.com/search?q=site:en.wikipedia.org+";
